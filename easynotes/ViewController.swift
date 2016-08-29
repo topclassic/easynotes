ff35f18a27b9ee7a356c93b49805385d2d9f2c7e
Chotipat Poowongthanarat <top_classic@hotmail.com>
2016-08-29 21:46:46 +0700

    Initial Commit

---
+++ easynotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  easynotes
+//
+//  Created by Chotipat on 8/29/2559 BE.
+//  Copyright © 2559 Chotipat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
