581f4993c0ad6f759b59b37d792c337fbd719554
Chotipat Poowongthanarat <top_classic@hotmail.com>
2016-09-01 12:41:02 +0700

    Import GoogleAnalytics Framework

--- easynotes/ViewController.swift
+++ easynotes/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import RealmSwift
+import GoogleAnalytics
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
     override func didReceiveMemoryWarning() {
